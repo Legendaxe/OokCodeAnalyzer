fdb014dc2011b0061f1e198b629759590e869d9b
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-04-24 00:19:41 +0300

    Lexical Analyzer without state machine.

--- Analyzer/Analyzer.cs
+++ Analyzer/Analyzer.cs
@@ -1,6 +1,49 @@
-﻿namespace Analyzer;
+﻿using OokCodeAnalyzer.Core;
+using Synthesizer;
+
+namespace Analyzer;
 
 public class Analyzer
 {
+
+    private enum State
+    {
+        
+    }
     
+    public static void Main()
+    {
+        ScrapTextForLexemes(Synthesizer.Synthesizer.SynthesizeOokCode(5) + "asdasdsdfsadfs" + Synthesizer.Synthesizer.SynthesizeOokCode(5));
+    }
+    public static IEnumerable<string> ScrapTextForLexemes(string text)
+    {
+        List<string> lexemes = new List<string>();
+        string lexeme;
+        
+        text = text.ReplaceLineEndings("").Replace(" ", "");
+        int i = 0;
+        while (i < text.Length)
+        {
+            if (i + 9 > text.Length)
+            {
+                lexemes.Add($"Error occured, this is not a lexeme: {text.Substring(i)}");
+                break;
+            }
+
+            lexeme = text.Substring(i, 9);
+            if (OokWord.IsOokWord(lexeme))
+            {
+                lexemes.Add($"Lexeme: \"{lexeme}\" #{lexemes.Count + 1}");
+            }
+            else
+            {
+                lexemes.Add($"Error occured, this is not a lexeme: {lexeme}");
+                break;
+            }
+
+            i += 9;
+        }
+
+        return lexemes;
+    }
 }
