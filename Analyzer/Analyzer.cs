c4db996d7cc134a29da338fdae6fe0652de819c3
Legendaxe <102746941+Legendaxe@users.noreply.github.com>
2023-05-05 22:39:22 +0300

    Cleanup

--- Analyzer/Analyzer.cs
+++ Analyzer/Analyzer.cs
@@ -15,43 +15,41 @@ public static class Analyzer
 
         int i = 1;
 
-        using (StringReader stringReader = new StringReader(text))
+        using StringReader stringReader = new StringReader(text);
+        while (stringReader.Read(ch, 0, 1) >= 1 && state != OokStateMachine.State.Error)
         {
-            while (stringReader.Read(ch, 0, 1) >= 1 && state != OokStateMachine.State.Error)
+            state = ookStateMachine[state].Item1(ch[0]);
+            switch (state)
             {
-                state = ookStateMachine[state].Item1(ch[0]);
-                switch (state)
-                {
-                    case OokStateMachine.State.OokDOokD:
-                        lexemes.Add($"Lexeme: \"Ook. Ook.\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokDOokE:
-                        lexemes.Add($"Lexeme: \"Ook. Ook!\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokDOokQ:
-                        lexemes.Add($"Lexeme: \"Ook. Ook?\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokEOokD:
-                        lexemes.Add($"Lexeme: \"Ook! Ook.\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokEOokE:
-                        lexemes.Add($"Lexeme: \"Ook! Ook!\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokEOokQ:
-                        lexemes.Add($"Lexeme: \"Ook! Ook?\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokQOokD:
-                        lexemes.Add($"Lexeme: \"Ook? Ook.\" #{i++}.");
-                        break;
-                    case OokStateMachine.State.OokQOokE:
-                        lexemes.Add($"Lexeme: \"Ook? Ook!\" #{i++}.");
-                        break;
-                }
+                case OokStateMachine.State.OokDOokD:
+                    lexemes.Add($"Lexeme: \"Ook. Ook.\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokDOokE:
+                    lexemes.Add($"Lexeme: \"Ook. Ook!\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokDOokQ:
+                    lexemes.Add($"Lexeme: \"Ook. Ook?\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokEOokD:
+                    lexemes.Add($"Lexeme: \"Ook! Ook.\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokEOokE:
+                    lexemes.Add($"Lexeme: \"Ook! Ook!\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokEOokQ:
+                    lexemes.Add($"Lexeme: \"Ook! Ook?\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokQOokD:
+                    lexemes.Add($"Lexeme: \"Ook? Ook.\" #{i++}.");
+                    break;
+                case OokStateMachine.State.OokQOokE:
+                    lexemes.Add($"Lexeme: \"Ook? Ook!\" #{i++}.");
+                    break;
             }
-            if (state == OokStateMachine.State.Error)
-            {
-                lexemes.Add("Error occured"); 
-            }
+        }
+        if (state == OokStateMachine.State.Error)
+        {
+            lexemes.Add("Error occured"); 
         }
 
         return lexemes;
